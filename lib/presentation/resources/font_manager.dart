6ebbe117106d3f747551f3202587c5111829ab25
Amr Fatouh <amrfatouh1499@gmail.com>
2022-04-16 05:15:10 +0200

    adding to font boilerplate

--- lib/presentation/resources/font_manager.dart
+++ lib/presentation/resources/font_manager.dart
@@ -1,12 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 class FontConstants {
   static const String tajawal = "Tajawal";
 }
 
-class FontSize {
-  static const double s12 = 12.0;
-  static const double s14 = 14.0;
-  static const double s16 = 16.0;
-  static const double s17 = 17.0;
-  static const double s18 = 18.0;
-  static const double s20 = 20.0;
+class FontSizeManager {
+  static double s13 = 13.sp;
+  static double s14 = 14.sp;
+  static double s15 = 15.sp;
+  static double s16 = 16.sp;
+  static double s18 = 18.sp;
+  static double s20 = 20.sp;
+  static double s22 = 22.sp;
+}
+
+class FontWeightManager {
+  static const FontWeight w300 = FontWeight.w300;
+  static const FontWeight w400 = FontWeight.w400;
+  static const FontWeight w500 = FontWeight.w500;
+  static const FontWeight w700 = FontWeight.w700;
+  static const FontWeight w800 = FontWeight.w800;
+  static const FontWeight w900 = FontWeight.w900;
 }
