96652215e16824decad770eb3add10c1ba9ae23f
Amr Fatouh <amrfatouh1499@gmail.com>
2022-07-06 04:41:08 +0200

    catching more crashes with firebase crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
@@ -12,29 +14,32 @@ import 'package:urrevs_ui_mobile/presentation/resources/language_manager.dart';
 import 'package:urrevs_ui_mobile/translations/codegen_loader.g.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
-  await EasyLocalization.ensureInitialized();
-  await initAppModule();
+  runZonedGuarded<Future<void>>(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+    await EasyLocalization.ensureInitialized();
+    await initAppModule();
 
+    final PendingDynamicLinkData? initialLink =
+        await FirebaseDynamicLinks.instance.getInitialLink();
 
-  final PendingDynamicLinkData? initialLink =
-      await FirebaseDynamicLinks.instance.getInitialLink();
-
-  runApp(
-    EasyLocalization(
-      supportedLocales:
-          LanguageType.values.map((langType) => langType.locale).toList(),
-      startLocale: LanguageType.ar.locale,
-      path: AssetsPaths.translations,
-      fallbackLocale: LanguageType.en.locale,
-      assetLoader: CodegenLoader(),
-      child: ProviderScope(child: MyApp(initialLink: initialLink)),
-    ),
-    // MyApp(),
-  );
+    runApp(
+      EasyLocalization(
+        supportedLocales:
+            LanguageType.values.map((langType) => langType.locale).toList(),
+        startLocale: LanguageType.ar.locale,
+        path: AssetsPaths.translations,
+        fallbackLocale: LanguageType.en.locale,
+        assetLoader: CodegenLoader(),
+        child: ProviderScope(child: MyApp(initialLink: initialLink)),
+      ),
+      // MyApp(),
+    );
+  }, (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+  });
 }
