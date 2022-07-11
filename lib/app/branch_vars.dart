ce1186cb0bd565a96988ece7f5d0e36bc75d89dc
Amr Fatouh <amrfatouh1499@gmail.com>
2022-07-11 21:21:11 +0200

    adjusting main branch links

--- lib/app/branch_vars.dart
+++ lib/app/branch_vars.dart
@@ -1,8 +1,8 @@
 import 'package:urrevs_ui_mobile/presentation/resources/strings_manager.dart';
 
 class BranchVars {
-  static const String currentBackendApi = StringsManager.herokuBackendApi;
-  static const String webDomain = 'urrevstest.netlify.app';
-  static const String uriPrefix = 'https://urevs.page.link';
-  static const String mockApiUrl = 'http://10.0.2.2:3000/';
+  static const String currentBackendApi = StringsManager.awsBackendApi;
+  static const String webDomain = 'urrevs.com';
+  static const String uriPrefix = 'https://urrevs.page.link';
+  static const String mockApiUrl = 'http://10.0.2.2:3001/';
 }
